f677554195ac394f1f82ff27ac7a9d3757770dd3
carlosv3l3zz <cvelezechavarria@gmail.com>
2025-06-09 02:05:56 -0500

    Actualiza el diseño de la página de inicio y el login, cambiando la fuente a Poppins y mejorando la interacción con un efecto de hover en el botón. Se eliminan referencias a la fuente Inter y se añaden nuevas clases de estilo en CSS.

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import "@/components/script.jsx";
+
 export const Login = () => {
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="w-screen h-screen flex justify-between items-center bg-fondo">
-
+    <div className={`w-screen h-screen flex justify-between items-center gradient ${isHovered ? 'active' : ''}`}>
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <button 
+          className="bg-blanco justify-center items-center rounded-[10px] hover:bg-[#000] transition-all duration-400 sombra z-10"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          <h3 className="fondo2 hover:text-[#b60000] py-3 px-4 flex text-center transition-all duration-400">
+            <span className="letter">C</span>
+            <span className="letter">L</span>
+            <span className="letter">I</span>
+            <span className="letter">C</span>
+            <span className="letter">K</span>
+            <span className="letter ml-2.5">M</span>
+            <span className="letter">E</span>
+          </h3>
+        </button>
+      </div>
     </div>
   );
 };
